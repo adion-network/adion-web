f50a2c9356e9fc92bebb74bceb05bc8886355d55
Patchs <paqi@cobra37.com>
2024-03-21 14:54:13 +0800

    font family

--- src/app/theme.tsx
+++ src/app/theme.tsx
@@ -4,6 +4,21 @@ import { grey } from "@mui/material/colors"
 import { createTheme } from "@mui/material/styles"
 
 export const darkTheme = createTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif",
+    ].join(","),
+  },
   palette: {
     mode: "dark",
     background: {
